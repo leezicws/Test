fb7834e9bcf3b1c2ddfe899507c088b3c311bf3c
chenws <leezicws@163.com>
2015-07-24 20:32:39 +0800

    1111

---
+++ link.php
@@ -0,0 +1,70 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+<title>File-Save 2009</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<style type="text/css">
+<!--
+body,td,th {color: #004080;font-family:Verdana;font-size:12px;}
+body {background-color: #888888;}
+a{color: #004080;text-decoration: none;}
+a:hover{text-decoration:underline;}
+.vd{width:730px;background:white;border-top:0px;border-left:solid 1px #003366;border-right:solid 1px #003366;border-bottom:solid 1px #003366;}
+.vt{width:550px;border:solid 1px #6D6D6D;text-align:left;padding:5px;margin-top:5px;}
+.vud{float:right;width:100px;text-align:right;padding-right:10px;}
+-->
+</style>
+</head>
+<body>
+<BR>
+
+<SCRIPT LANGUAGE="JavaScript">
+setstop = 1;
+function setpos(str){
+	window.open('http://www.tiaotiaolele.com');
+}
+</SCRIPT>
+
+<div align="center">
+<TABLE cellspacing=1 cellpadding=1 width='800' bgcolor=#003366>
+<TR>
+	<TD bgcolor="#CFE2CD" height="50" ><span style="font-size:24px"><B>File-Save 2009</B></span></TD>
+</TR>
+<TR>
+	<FORM action='download.php' method='post' encType='multipart/form-data'>
+	<TD bgcolor="white" valign="top" style="padding:10px;">
+	<TABLE cellspacing=0 cellpadding=0 width='100%'>
+	<TR>
+		<TD width='30%' align='center'>
+			Uploaded torrent code (date+hash):<BR>
+			<INPUT size=58 name="ref" value="1510100e4f48deadde1ab0b312bb261f8f39e65cbb1" style="font-size:10px;"><INPUT TYPE="hidden" NAME="reff" value="MTQyNDUzNjEwOA=="><BR>
+			downloaded: 6962<BR>
+			<INPUT type="submit" height=27 width=174 value=download border=0 name=submit valign="bottom" onclick="setpos(1);">
+		</TD>
+		<TD width='70%'><a href='http://www.cliushow.com' target='_blank'><img src='http://www.nextcps.com/pic/filesave1.gif' border='0' width='468' height='60'></a></TD>
+	</TR>
+	</TABLE>
+	</TD>
+	</FORM>
+</TR>
+</TABLE>
+<TABLE cellspacing=1 cellpadding=1 width='800' bgcolor=#003366>
+<TR>
+	<TD bgcolor="white">
+		<BR><BR>
+		<CENTER>
+<a href='http://www.twfuwu.info/zi' target='_blank'><img src='http://www.twfuwu.info/pc1.gif' border='0' width='760' height='90'></a><BR><BR>
+<a href='http://www.twfuwu.info/zi2' target='_blank'><img src='http://www.twfuwu.info/pc2.gif' border='0' width='760' height='90'></a>
+<BR><BR>
+		</CENTER>
+		<BR><BR>
+	</TD>
+</TR>
+</TABLE>
+<CENTER>
+</CENTER>
+
+Illegal torrents report: please <a href="ticket.php">click here</a>, we will remove it asap, thanks!<BR>
+Copyright @ 2007-2009 <a href="./">Torrent File Save</a>. All rights reserved (Version 2.1)
+</body>
+</html>
